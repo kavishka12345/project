0c963eb4fc050f43fa408f1aa55454e73e59df21
user <krasasnjana81@gmail.com>
2024-06-11 15:28:03 +0530

    my project i need to pus git git hub

---
+++ src/hllo/Hllo.java
@@ -0,0 +1,29 @@
+/*
+my project i need to pus git git hub
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hllo;
+
+/**
+ *
+ * @author user
+ */
+public class Hllo {
+int t=45;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a;
+       int x=5;
+       int y=10;
+       int z=15;
+       a=x+y+z;
+       
+       int  b =a/5;
+        System.out.println(b);
+    }
+    
+}
